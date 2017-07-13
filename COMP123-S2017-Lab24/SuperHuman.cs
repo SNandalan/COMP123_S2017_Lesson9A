e190d93fd14e2133afe60899ca6789c1eeb146fc
SNandalan <snandal2@my.centennialcollege.ca>
2017-07-13 12:14:41 -0400

    Re-added SuperHuman class

--- COMP123-S2017-Lab24/SuperHuman.cs
+++ COMP123-S2017-Lab24/SuperHuman.cs
@@ -3,49 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-/*Name: Sushmita Nandalan
- * Date: July 11th 2017
- * Description: This is the SuperHuman class that extends to the Human class
- * Version: 0.7 - Added a DisplaySkills() stub method to satify the Human superclass  method. 
+/*
+ * Name: Sushmita Nandalan
+ * Date: July 13, 2017
+ * Description: This is the SuperHuman class that extends the Human abstract class
+ * Version: 0.7 - Added the DisplaySkills stub method to satisfy the Human super class requirements
  */
 namespace COMP123_S2017_Lab24
 {
-    /// <summary>
-    /// This is the SuperHuman sub class
-    /// It intherits from the Human SuperClass
+    // <summary>
+    /// This is the SuperHuman class.
     /// </summary>
     public class SuperHuman : Human
     {
-        // PRIVATE FIELDS
+        // PRIVATE INSTANCE VARIABLES
         private List<Power> _powers;
 
-        //PUBLIC PROPERTIES
-       public List<Power> Powers
+        // PUBLIC PROPERTIES
+        public List<Power> Powers
         {
             get
             {
-                return this._powers; // returns a reference to the Powers List
+                return this._powers; // this returns a reference
             }
         }
 
-        //CONSTRUCTORS
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the SuperHuman class.
+        /// It takes one parameter - name (string)
+        /// </summary>
+        /// <param name="name"></param>
         public SuperHuman(string name)
-            :base(name)
+            : base(name)
         {
             this._initialize();
         }
-        //PRIVATE METHODS
+
+        // PRIVATE METHODS
+
         /// <summary>
-        /// This method initializes and assigns default values to the Class fields.
+        /// This method initializes, instantiates and assigns values to class properties
         /// </summary>
         private void _initialize()
         {
-            this._powers = new List<Power>();
+            this._powers = new List<Power>(); // creates an empty list
         }
-        //PUBLIC METHODS
+
+        // PUBLIC METHODS
+
         /// <summary>
-        /// This method adds a Power to the Power list
+        /// This method adds a power to the Power List.
+        /// It takes two parameters - name (string) - rank (int)
         /// </summary>
         /// <param name="name"></param>
         /// <param name="rank"></param>
@@ -53,35 +63,40 @@ namespace COMP123_S2017_Lab24
         {
             this.Powers.Add(new Power(name, rank));
         }
+
+        /// <summary>
+        /// This method displays each of the Powers stored in the Powers List
+        /// </summary>
         public void DisplayPowers()
         {
-            foreach (var power in Powers)
+            foreach (Power power in this.Powers)
             {
-                Console.WriteLine("Name: " + power.Name + " Rank: " +power.Rank);
+                Console.WriteLine("Power: " + power.Name + " Rank: " + power.Rank);
             }
         }
+
         /// <summary>
-        ///The ToString method overrides the built-in Object.ToString method.
+        /// The ToString method overrides the built-in Object.ToString method.
         /// It outputs a string that displays the superhuman's name and their power list.
         /// </summary>
         /// <returns></returns>
         public override string ToString()
         {
             string outputString = "";
-            outputString += "*****************************************************************************************\n";
+            outputString += "===========================================================\n";
             outputString += "Super Human Name: " + this.Name + "\n";
-            outputString += "*****************************************************************************************\n";
+            outputString += "===========================================================\n";
             foreach (Power power in this.Powers)
             {
-                outputString += "Power: " + power.Name + "Rank: " + power.Rank + "\n";
+                outputString += "Power: " + power.Name + " Rank: " + power.Rank + "\n";
             }
-            outputString += "*****************************************************************************************\n";
+            outputString += "===========================================================\n";
             return outputString;
         }
 
         /// <summary>
-        /// This method overrides the Human abstract superclass DisplaySkills() method
-        /// This method will display a list of skills to the console.
+        /// This method overrides the Human abstract superclass DisplaySkills method.
+        /// This method will display a list of Skills to the Console.
         /// </summary>
         public override void DisplaySkills()
         {
