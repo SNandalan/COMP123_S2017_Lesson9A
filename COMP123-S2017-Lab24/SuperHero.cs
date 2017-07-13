d69282e7a93f130cc3d4d976d8a4de0f509d4318
SNandalan <snandal2@my.centennialcollege.ca>
2017-07-13 11:50:49 -0400

    Added SuperHero subclass

---
+++ COMP123-S2017-Lab24/SuperHero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2017_Lab24
+{
+    /// <summary>
+    /// This is the SuperHero class
+    /// </summary>
+    public class SuperHero : SuperHuman, IHasKarma
+    {
+        //  PRIVATE INSTANCE VARIABLES
+        private int _karma;
+
+        //PUBLIC PROPERTIES
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+            }
+            set
+            {
+                this._karma = value;
+            }
+        }
+
+        //CONSTRUCTORS
+        /// <summary>
+        /// This is the main constructor for the superHuman class
+        /// It takes 2 arguments - name(string) and karma
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="karma"></param>
+        public SuperHero(string name, int karma)
+            :base(name)
+        {
+            this.Karma = karma;
+        }
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
