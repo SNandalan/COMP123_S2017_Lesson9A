197e0aa7c13b58a4679e6acf111863e74de819a9
SNandalan <snandal2@my.centennialcollege.ca>
2017-07-13 11:39:35 -0400

    Added IHasKarma interface and declared the Karma property.

---
+++ COMP123-S2017-Lab24/IHasKarma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Name: Sushmita Nandalan
+ * Date: July 13th 2017
+ * Description: This Interface lists the signature for any Super Hero subclass
+ * Version: 0.1 - Declared the Karma property 
+ */
+namespace COMP123_S2017_Lab24
+{
+        public interface IHasKarma
+        {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+        int Karma { get; set; }
+    }
+
+}
